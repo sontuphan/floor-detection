2ce323a041981cf440f2e44c3e6264de7ed953cd
tphanson <tphanson@kambria.io>
2020-07-27 16:08:10 +0700

    debug

--- src/dataset.py
+++ src/dataset.py
@@ -37,9 +37,8 @@ class Dataset:
             img = tf.image.flip_left_right(img)
             mask = tf.image.flip_left_right(mask)
         if tf.random.uniform(()) > 0.5:
-            print('shit')
-        #     img = tf.image.central_crop(img, central_fraction=0.8)
-        #     img = tf.image.resize(img, self.image_shape)
+            img = tf.image.central_crop(img, central_fraction=0.8)
+            img = tf.image.resize(img, self.image_shape)
         #     mask = tf.image.central_crop(mask, central_fraction=0.8)
         #     mask = tf.image.resize(mask, self.image_shape)
         return img, mask
