4946c3bcd6ee322c3f94321e0e8be7a977176d58
tphanson <tphanson@kambria.io>
2020-07-27 14:24:32 +0700

    debug

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from test import dataset, detector
 # https://github.com/tensorflow/tensorflow/issues/36510
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
-    # Restrict TensorFlow to only use the first GPU
     try:
-        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
     except RuntimeError as e:
-        # Visible devices must be set before GPUs have been initialized
+        # Memory growth must be set before GPUs have been initialized
         print(e)
 
 if __name__ == "__main__":
